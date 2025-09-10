61d4a0b9427d5191a18d4c663208e9db530d0941
Amir Rasteg <amir.rasteg@protonmail.com>
2025-09-10 18:08:02 -0400

    Identity Stages

--- src/feagi_connector_core/data_pipeline/stages/identities.rs
+++ src/feagi_connector_core/data_pipeline/stages/identities.rs
@@ -1,48 +1,81 @@
-use feagi_connector_core::data_pipeline::PipelineStage;
 use feagi_connector_core::data_pipeline::stages::*;
-use feagi_data_structures::FeagiDataError;
-use pyo3::{pyclass, pymethods, PyResult, Py};
-use pyo3::exceptions::{PyNotImplementedError, PyValueError};
+use pyo3::{pyclass, pymethods, PyResult,};
+use pyo3::exceptions::{PyValueError};
 use pyo3::prelude::*;
 use crate::{project_display, py_type_casts};
 use crate::feagi_connector_core::data_pipeline::pipeline_stage::PyPipelineStage;
-use crate::feagi_data_structures::wrapped_io_data::PyWrappedIOType;
+use crate::feagi_data_structures::data::{PyImageFrame, PySegmentedImageFrame};
 
+//region Identity Float
 #[pyclass(str, extends=PyPipelineStage)]
 #[pyo3(name = "IdentityFloatStage")]
 #[derive(Clone)]
-pub struct PyIdentityFloatStage {
-    inner: IdentityFloatStage,
-}
+pub struct PyIdentityFloatStage;
 
 #[pymethods]
 impl PyIdentityFloatStage {
 
     #[new]
-    pub fn new(initial_value: f32) -> PyResult<(PyIdentityFloatStage, PyPipelineStage)> {
-        Ok((
-            PyIdentityFloatStage {
-                inner: IdentityFloatStage::new(initial_value).map_err(|e| PyValueError::new_err(format!("{:?}", e)))?,
-            },
-            PyPipelineStage {}
-        ))
+    pub fn new(initial_value: f32) -> PyResult<(Self, PyPipelineStage)> {
+        let result_stage = IdentityFloatStage::new(initial_value);
+        if result_stage.is_err() {
+            return Err(PyValueError::new_err(format!("{:?}", result_stage.err().unwrap())))
+        }
+        Ok((PyIdentityFloatStage, PyPipelineStage::new(Box::new(result_stage.unwrap()))))
     }
-
-
-
 }
-
- impl PipelineStagePyTrait for PyIdentityFloatStage {
-     fn copy_as_box(&self) -> Result<Box<dyn PipelineStage>, FeagiDataError> {
-         let stage = self.inner.clone();
-        Ok(Box::new(stage))
-     }
- }
-
-
-
 
 project_display!(PyIdentityFloatStage);
 py_type_casts!(PyIdentityFloatStage, IdentityFloatStage);
 
+//endregion
 
+
+//region Identity Image Frame
+#[pyclass(str, extends=PyPipelineStage)]
+#[pyo3(name = "IdentityImageFrameStage")]
+#[derive(Clone)]
+pub struct PyIdentityImageFrameStage;
+
+#[pymethods]
+impl PyIdentityImageFrameStage {
+
+    #[new]
+    pub fn new(initial_image: PyImageFrame) -> PyResult<(Self, PyPipelineStage)> {
+        let result_stage = IdentityImageFrameStage::new(initial_image.into());
+        if result_stage.is_err() {
+            return Err(PyValueError::new_err(format!("{:?}", result_stage.err().unwrap())))
+        }
+        Ok((PyIdentityImageFrameStage, PyPipelineStage::new(Box::new(result_stage.unwrap()))))
+    }
+}
+
+project_display!(PyIdentityImageFrameStage);
+py_type_casts!(PyIdentityImageFrameStage, IdentityImageFrameStage);
+
+//endregion
+
+
+//region Identity Segmented Image Frame
+#[pyclass(str, extends=PyPipelineStage)]
+#[pyo3(name = "IdentitySegmentedImageFrameStage")]
+#[derive(Clone)]
+pub struct PyIdentitySegmentedImageFrameStage;
+
+#[pymethods]
+impl PyIdentitySegmentedImageFrameStage {
+
+    #[new]
+    pub fn new(initial_images: PySegmentedImageFrame) -> PyResult<(Self, PyPipelineStage)> {
+        let result_stage = IdentitySegmentedImageFrameStage::new(initial_images.into());
+        if result_stage.is_err() {
+            return Err(PyValueError::new_err(format!("{:?}", result_stage.err().unwrap())))
+        }
+        Ok((PyIdentitySegmentedImageFrameStage, PyPipelineStage::new(Box::new(result_stage.unwrap()))))
+    }
+}
+
+project_display!(PyIdentitySegmentedImageFrameStage);
+py_type_casts!(PyIdentitySegmentedImageFrameStage, IdentitySegmentedImageFrameStage);
+
+//endregion
