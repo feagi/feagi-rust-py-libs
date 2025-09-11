2190012428a8e7534f1cac91b6a4aae36d841c39
Amir Rasteg <amir.rasteg@protonmail.com>
2025-09-11 18:38:34 -0400

    update quick diff

--- src/feagi_connector_core/data_pipeline/stages/image_quick_diff.rs
+++ src/feagi_connector_core/data_pipeline/stages/image_quick_diff.rs
@@ -1,4 +1,6 @@
+use std::ops::{Range, RangeInclusive};
 use feagi_connector_core::data_pipeline::stages::ImageFrameQuickDiffStage;
+use feagi_data_structures::basic_components::Percentage;
 use feagi_data_structures::data::image_descriptors::ImageFrameProperties;
 use pyo3::{pyclass, pymethods, PyResult};
 use pyo3::exceptions::{PyValueError};
@@ -15,9 +17,14 @@ pub struct PyImageFrameQuickDiffStage;
 #[pymethods]
 impl PyImageFrameQuickDiffStage {
     #[new]
-    pub fn new(image_properties: PyImageFrameProperties, threshold: u8) -> PyResult<(PyImageFrameQuickDiffStage, PyPipelineStage)> {
+    pub fn new(image_properties: PyImageFrameProperties, per_pixel_allowed_range: (u8, u8), acceptable_amount_of_activity_percentage_in_image:(f32, f32)) -> PyResult<(PyImageFrameQuickDiffStage, PyPipelineStage)> {
+        let per_pixel_allowed_range:RangeInclusive<u8> = per_pixel_allowed_range.0..=per_pixel_allowed_range.1;
+        let acceptable_amount_of_activity_in_image: RangeInclusive<Percentage> = RangeInclusive::new(
+            Percentage::new_from_0_1(acceptable_amount_of_activity_percentage_in_image.0).unwrap(),
+            Percentage::new_from_0_1(acceptable_amount_of_activity_percentage_in_image.1).unwrap(),
+        );
         let image_properties: ImageFrameProperties = image_properties.into();
-        let result_stage = ImageFrameQuickDiffStage::new(image_properties, threshold);
+        let result_stage = ImageFrameQuickDiffStage::new(image_properties, per_pixel_allowed_range, acceptable_amount_of_activity_in_image);
         if result_stage.is_err() {
             return Err(PyValueError::new_err(format!("{:?}", result_stage.err().unwrap())))
         }
