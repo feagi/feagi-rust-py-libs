3c9f81c9371206a33a3b7a2513ffb95d5d0fb9da
Amir Rasteg <amir.rasteg@protonmail.com>
2025-09-18 23:47:42 -0400

    added

--- src/feagi_data_structures/genomic/mod.rs
+++ src/feagi_data_structures/genomic/mod.rs
@@ -4,4 +4,4 @@ mod cortical_id;
 pub mod descriptors;
 
 pub use cortical_id::PyCorticalID;
-pub use cortical_type::{PyCorticalType, PySensorCorticalType, PyCoreCorticalType};
+pub use cortical_type::{PyCorticalType, PySensorCorticalType, PyCoreCorticalType, PyMotorCorticalType};
--- src/lib.rs
+++ src/lib.rs
@@ -134,6 +134,7 @@ fn feagi_rust_py_libs(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     add_python_class!(py, m, "connector_core.data_pipeline.stages", feagi_connector_core::data_pipeline::stages::PyIdentityFloatStage);
     add_python_class!(py, m, "connector_core.data_pipeline.stages", feagi_connector_core::data_pipeline::stages::PyIdentityImageFrameStage);
     add_python_class!(py, m, "connector_core.data_pipeline.stages", feagi_connector_core::data_pipeline::stages::PyIdentitySegmentedImageFrameStage);
+    add_python_class!(py, m, "connector_core.data_pipeline.stages", feagi_connector_core::data_pipeline::stages::PyIdentitySegmentedImageFrameStage);
     add_python_class!(py, m, "connector_core.data_pipeline.stages", feagi_connector_core::data_pipeline::stages::PyImageFrameProcessorStage);
     add_python_class!(py, m, "connector_core.data_pipeline.stages", feagi_connector_core::data_pipeline::stages::PyImageFrameSegmentatorStage);
     add_python_class!(py, m, "connector_core.data_pipeline.stages", feagi_connector_core::data_pipeline::stages::PyImagePixelValueCountThresholdStage);
