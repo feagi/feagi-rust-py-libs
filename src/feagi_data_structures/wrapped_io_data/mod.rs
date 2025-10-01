365431417c5b5c4c3719500950106e947f0d24ee
Amir Rasteg <amir.rasteg@protonmail.com>
2025-10-01 13:38:48 -0400

    update wrapped data

--- src/feagi_data_structures/wrapped_io_data/mod.rs
+++ src/feagi_data_structures/wrapped_io_data/mod.rs
@@ -1,8 +1,8 @@
-mod WrappedIOType;
-mod WrappedIOData;
+mod wrapped_iotype;
+mod wrapped_iodata;
 
 
 // Re-export all the public structs
-pub use WrappedIOType::*;
-pub use WrappedIOData::*;
+pub use wrapped_iotype::*;
+pub use wrapped_iodata::*;
 
