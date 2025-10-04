a76f55833f51a0ade896c01a626e59861f23e410
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2025-10-04 14:46:39 -0400

    misc

---
+++ src/feagi_data_serialization/byte_structure/mod.rs
@@ -0,0 +1,6 @@
+mod feagi_byte_structure;
+mod feagi_byte_structure_compatible;
+
+pub use feagi_byte_structure::PyFeagiByteStructure;
+pub use feagi_byte_structure_compatible::PyFeagiByteStructureCompatible;
+
