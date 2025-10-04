c4a6d279bbb03d2657d0776563db41bda8778271
Mohammad Nadji-Tehrani <26581495+m-nadji-tehrani@users.noreply.github.com>
2025-10-04 16:16:21 -0400

    misc

--- src/feagi_data_serialization/byte_structure/mod.rs
+++ src/feagi_data_serialization/byte_structure/mod.rs
@@ -1,6 +1,7 @@
-mod feagi_byte_structure;
-mod feagi_byte_structure_compatible;
+// Disabled: depends on types not exported in published feagi_data_serialization 0.0.50-beta.33
+// mod feagi_byte_structure;
+// mod feagi_byte_structure_compatible;
 
-pub use feagi_byte_structure::PyFeagiByteStructure;
-pub use feagi_byte_structure_compatible::PyFeagiByteStructureCompatible;
+// pub use feagi_byte_structure::PyFeagiByteStructure;
+// pub use feagi_byte_structure_compatible::PyFeagiByteStructureCompatible;
 
--- src/feagi_data_serialization/mod.rs
+++ src/feagi_data_serialization/mod.rs
@@ -1,9 +1,9 @@
-mod byte_structure;
+// mod byte_structure;  // Disabled: depends on unavailable exports
 mod feagi_byte_container;
 mod feagi_serializable;
 mod feagi_byte_structure_type;
 
-pub use byte_structure::PyFeagiByteStructure;
+// pub use byte_structure::PyFeagiByteStructure;  // Disabled
 pub use feagi_byte_container::PyFeagiByteContainer;
 pub use feagi_byte_structure_type::PyFeagiByteStructureType;
 pub use feagi_serializable::PyFeagiSerializable;
