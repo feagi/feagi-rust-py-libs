813ef8644d96f49e7544b5b74ea6b3d5cabf3649
Amir Rasteg <amir.rasteg@protonmail.com>
2025-12-03 20:18:08 -0500

    complete main conversion!

--- src/feagi_data_structures/neurons_voxels/xyzp/neuron_voxel_xyzp.rs
+++ src/feagi_data_structures/neurons_voxels/xyzp/neuron_voxel_xyzp.rs
@@ -1,21 +1,12 @@
 use std::fmt::Debug;
-use pyo3::prelude::*;
+
 use pyo3::{pyclass, pymethods};
+use pyo3::prelude::*;
 use feagi_data_structures::FeagiDataError;
 use feagi_data_structures::neuron_voxels::xyzp::NeuronVoxelXYZP;
+use crate::{create_pyclass, __base_py_class_shared};
 
-use crate::{create_pyclass_with_clone_equal, __base_py_class_shared, __pyclass_from_py_object};
-use crate::{create_pyclass, create_pyclass_with_equal};
-
-
-/*
-//region NeuronVoxelXYZP Implementation
-#[pyclass(str)]
-#[derive(Clone)]
-#[pyo3(name = "NeuronVoxelXYZP")]
-pub struct PyNeuronVoxelXYZP {
-    pub inner: NeuronVoxelXYZP,
-}
+create_pyclass!(PyNeuronVoxelXYZP, NeuronVoxelXYZP, "PyNeuronVoxelXYZP");
 
 #[pymethods]
 impl PyNeuronVoxelXYZP {
@@ -28,53 +19,3 @@ impl PyNeuronVoxelXYZP {
         self.inner.as_tuple()
     }
 }
-//endregion
-
-py_type_casts!(PyNeuronVoxelXYZP, NeuronVoxelXYZP);
-project_display!(PyNeuronVoxelXYZP);
-
- */
-
-#[pyclass(str)]
-#[pyo3(name = "PyNeuronVoxelXYZP")]
-pub struct PyNeuronVoxelXYZP {
-    pub inner: NeuronVoxelXYZP,
-}
-
-impl std::fmt::Display for PyNeuronVoxelXYZP {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.inner.to_string())
-    }
-}
-
-/*
-// Rust -> Python Clone
-impl IntoPy<pyo3::PyAny> for PyNeuronVoxelXYZP {
-    fn into_py(self, py: pyo3::Python<'_>) -> pyo3::PyAny {
-        pyo3::Py::new(py, self).unwrap().into_py(py)
-    }
-}
-
- */
-
-impl PyNeuronVoxelXYZP {
-    /// Create Python wrapped instance of the given Rust structure
-    pub(crate) fn from_rust(rust_struct: NeuronVoxelXYZP) -> Self {
-        PyNeuronVoxelXYZP {inner: rust_struct}
-    }
-
-    /// Try to downcast a &PyAny into PyRef<Self>
-    pub fn try_as_py_ref<'py>(obj: pyo3::Bound<'py, PyAny>) -> Result<PyRef<'py, NeuronVoxelXYZP>, FeagiDataError> {
-        //let a =   obj.extract::<PyRef<'py, Self>>();
-        let result = obj.extract::<PyRef<'py, Self>>();
-        match result {
-            Ok(py_ref) => Ok(py_ref.inner),
-            Err(_) => Err(FeagiDataError::BadParameters(format!("Unable to extract {} from given python variable!", "NeuronVoxelXYZP")))
-        }
-    }
-}
-
-
-
-
-//create_pyclass!(PyNeuronVoxelXYZP, NeuronVoxelXYZP, "PyNeuronVoxelXYZP");
--- src/useful_macros/py_wrapping.rs
+++ src/useful_macros/py_wrapping.rs
@@ -22,24 +22,6 @@ macro_rules! create_pyclass {
 }
 
 /// Takes the Pyclass internal name, and the rust type, to crate a basic
-/// wrapper of the rust struct as inner. Includes try_into from PyAny if the source has Clone
-#[macro_export]
-macro_rules! create_pyclass_with_clone {
-    ($py_wrapped_name:ident, $rust:ty, $py_name:expr) => {
-
-        #[pyclass(str)]
-        #[pyo3(name = $py_name)]
-        #[derive(Clone)]
-        pub struct $py_wrapped_name {
-            pub inner: $rust,
-        }
-
-        __base_py_class_shared!($py_wrapped_name, $rust, $py_name);
-        __pyclass_from_py_object!($py_wrapped_name);
-    };
-}
-
-/// Takes the Pyclass internal name, and the rust type, to crate a basic
 /// wrapper of the rust struct as inner. allows comparison if equal
 #[macro_export]
 macro_rules! create_pyclass_with_equal {
@@ -70,25 +52,6 @@ macro_rules! create_pyclass_with_hash {
     };
 }
 
-/// Takes the Pyclass internal name, and the rust type, to crate a basic
-/// wrapper of the rust struct as inner. Includes try_into from PyAny if the source has Clone.
-/// Allows comparison if equal
-#[macro_export]
-macro_rules! create_pyclass_with_clone_equal {
-    ($py_wrapped_name:ident, $rust:ty, $py_name:expr) => {
-
-        #[pyclass(str, eq)]
-        #[pyo3(name = $py_name)]
-        #[derive(Clone, PartialEq)]
-        pub struct $py_wrapped_name {
-            pub inner: $rust,
-        }
-
-        __base_py_class_shared!($py_wrapped_name, $rust, $py_name);
-        __pyclass_from_py_object!($py_wrapped_name);
-    };
-}
-
 //endregion
 
 //region Internal
@@ -106,40 +69,34 @@ macro_rules! __base_py_class_shared {
             }
         }
 
-        // Rust -> Python Clone
-        impl IntoPy<pyo3::PyObject> for $py_wrapped_name {
-            fn into_py(self, py: pyo3::Python<'_>) -> pyo3::PyObject {
-                pyo3::Py::new(py, self).unwrap().into_py(py)
-            }
-        }
 
         impl $py_wrapped_name {
             /// Create Python wrapped instance of the given Rust structure
-            pub(crate) fn from_rust(rust_struct: $rust:ty) -> Self {
+            pub(crate) fn new_from_rust(rust_struct: $rust) -> Self {
                 $py_wrapped_name {inner: rust_struct}
             }
 
-            /// Try to downcast a &PyAny into PyRef<Self>
-            pub fn try_as_pyref<'py>(obj: &'py PyAny) -> PyResult<PyRef<'py, Self>> {
-                obj.extract::<PyRef<'py, Self>>()
+            /// Static wrapping an existing rust non-py wrapped instance directly to PyAny
+            pub fn wrap_to_bound_any(py: Python<'_>, rust_struct: $rust) -> PyResult<Bound<'_, PyAny>> {
+                Bound::new(py, Self {inner: rust_struct} ).map(|b| b.into_any())
             }
+            /// Wraps self into into a Bound<PyAny>
+            pub fn wrap_self_into_bound_any(self, py: Python<'_>) -> PyResult<Bound<'_, PyAny>> {
+                Bound::new(py, self).map(|b| b.into_any())
+            }
+
+            /// Attempt to downcast as a reference to the python wrapper.
+            /// Use &ref.inner to borrow the rust data.
+            /// Note that doing if-elif-else chains will have to be the main way to get out a type from multiple.
+            pub fn try_extract_from_bound_any<'py>(obj: pyo3::Bound<'py, pyo3::PyAny>) -> Result<pyo3::PyRef<'py, Self>, feagi_data_structures::FeagiDataError> {
+                use pyo3::prelude::PyAnyMethods;
+                use feagi_data_structures::FeagiDataError;
+                obj.cast::<Self>()
+                    .map_err(|_| FeagiDataError::BadParameters(format!("Expected {} but got {:?}", $py_name, obj.get_type())))?
+                    .try_borrow()
+                    .map_err(|e| FeagiDataError::BadParameters(format!("Failed to borrow: {}", e)))
+            }
+
         }
     };
 }
-
-/// Requires Clone, allows try_into from a PyAny
-#[macro_export]
-macro_rules! __pyclass_from_py_object {
-    ($py_wrapped_name:ident) => {
-        impl<'py> pyo3::FromPyObject<'py> for $py_wrapped_name {
-            fn extract(obj: &'py pyo3::PyAny) -> pyo3::PyResult<Self> {
-                let py_ref: pyo3::PyRef<$py_wrapped_name> = obj.extract()?;
-                Ok(Self {
-                    inner: py_ref.inner.clone(),
-                })
-            }
-        }
-    };
-
-}
-//endregion
